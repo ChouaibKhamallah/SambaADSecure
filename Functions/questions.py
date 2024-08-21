28a5c2aefdedaf569e26bc95ef828278e08728f8
Chouaib Khamallah <chouaib.khamallah@univ-lille.fr>
2024-08-21 17:02:46 +0200

    test

---
+++ Functions/questions.py
@@ -0,0 +1,46 @@
+
+
+def get_user_choices(ScriptSettings):
+    user_choices = {}
+
+    for section in ScriptSettings["Questions"]:
+    
+        for question in ScriptSettings["Questions"][section]:
+            
+            if ScriptSettings["Questions"][section][question] == ["yes","no"]:
+                while True:
+                    user_input = input(f'{Fore.CYAN}{question} {Fore.WHITE}{ScriptSettings["Questions"][section][question]}: {Fore.GREEN}')
+                    if user_input.lower() in ["yes", "y"]:
+                        user_input = True
+                        break
+                    elif user_input.lower() in ["no", "n"]:
+                        user_input = False
+                        break
+                    else:
+                        print("Invalid input. Please enter yes/no.")
+
+            elif type(ScriptSettings["Questions"][section][question]) is dict:
+
+                print(f'{Fore.WHITE}{question}')
+
+                for option in ScriptSettings["Questions"][section][question]:
+                    print(f'{Fore.CYAN}{option} {Fore.YELLOW}{ScriptSettings["Questions"][section][question][option]}')
+
+                while True:
+                    user_input = input(f'{Fore.CYAN}Choose an option... {Fore.WHITE}{[x for x in ScriptSettings["Questions"][section][question]]}: {Fore.GREEN}')
+                    if user_input in [x for x in ScriptSettings["Questions"][section][question]]:
+                        break
+                    else:
+                        print("Invalid input, Please enter valid data")
+
+            else:
+                while True:
+                    user_input = input(f'{Fore.CYAN}{question} {Fore.WHITE}{ScriptSettings["Questions"][section][question]}: {Fore.GREEN}')
+                    if not user_input == "":
+                        break
+                    else:
+                        print("Invalid input, Please enter valid data")
+
+        user_choices[section] = user_input
+    
+    return user_choices
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import json
+from colorama import Fore, Back, Style
+from Functions import *
+
+with open('Configuration/ScriptSettings.json') as json_file:
+  ScriptSettings = json.load(json_file)
+
+print(ScriptSettings["Messages"]["Welcome!"])
